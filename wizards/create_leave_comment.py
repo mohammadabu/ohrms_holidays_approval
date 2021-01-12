1c1fcb1392ccac933ffab53d63e6655446b2ea02
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-01-12 15:35:20 +0200

    edit

--- wizards/create_leave_comment.py
+++ wizards/create_leave_comment.py
@@ -4,3 +4,62 @@ class CreateLeaveComment(models.TransientModel):
     _name = 'create.leave.comment'
     comment = fields.Text()
     
+    def create_comment(self):
+        view_id=self.env['create.leave.comment']
+        new = view_id.sudo().create({
+            'comment' :self.comment
+        })
+        current_uid = self.env.uid
+        current_employee = self.env['hr.employee'].search(
+            [('user_id', '=', self.env.uid)], limit=1)
+        li = {}
+        if self.env.context.get('active_id'):
+            active_id = self.env.context.get('active_id')
+        else:
+            active_id = self.id
+            
+        user = self.env['hr.leave'].search([('id', '=', active_id)], limit=1)
+        comment =  self.env['create.leave.comment'].sudo().search([('id', '=', new.id)], limit=1).comment 
+        for user_obj in user.leave_approvals:
+            if user_obj.validation_status != True:
+                if user_obj.validators_type == 'direct_manager' and user.employee_id.parent_id.id != False:
+                    if user.employee_id.parent_id.user_id.id != False:
+                        if user.employee_id.parent_id.user_id.id == current_uid:
+                            validation_obj = user.leave_approvals.search(
+                                    [('id', '=', user_obj.id)])
+                            validation_obj.validation_status = True
+                            validation_obj.validation_refused = False
+                            validation_obj.leave_comments = comment
+                if  user_obj.validators_type == 'position':
+                    employee = self.env['hr.employee'].sudo().search([('multi_job_id','in',user_obj.holiday_validators_position.id),('user_id','=',current_uid)])
+                    if len(employee) > 0:
+                        validation_obj = user.leave_approvals.search(
+                                    [('id', '=', user_obj.id)])
+                        validation_obj.validation_status = True
+                        validation_obj.validation_refused = False
+                        validation_obj.leave_comments = comment
+                if  user_obj.validators_type == 'user':
+                    if user_obj.holiday_validators_user.id == current_uid:
+                        validation_obj = user.leave_approvals.search(
+                                    [('id', '=', user_obj.id)])
+                        validation_obj.validation_status = True
+                        validation_obj.validation_refused = False
+                        validation_obj.leave_comments = comment
+                if not(user_obj.approval != True or (user_obj.approval == True and user_obj.validation_status == True)): 
+                    break 
+        approval_flag = True
+        for user_obj in user.leave_approvals:
+            if not user_obj.validation_status:
+                approval_flag = False
+        if approval_flag:
+            user.filtered(
+                lambda hol: hol.validation_type == 'both').sudo().write(
+                {'state': 'validate1',
+                 'first_approver_id': current_employee.id})
+            user.filtered(lambda
+                              hol: not hol.validation_type == 'both').sudo().action_validate()
+            if not user.env.context.get('leave_fast_create'):
+                user.activity_update()
+            return True
+        else:
+            return False
