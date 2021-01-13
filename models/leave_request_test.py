b92bcbf77db1280a94739d1c4265bc7b9f5e9147
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-01-13 11:34:24 +0200

    Update leave_request_test.py

--- models/leave_request_test.py
+++ models/leave_request_test.py
@@ -298,7 +298,7 @@ class HrLeave(models.Model):
     def create_header_for_email(self,holiday_status_id,employee_id):
         hr_holidays = self.env['hr.leave.type'].sudo().search([('id','=',holiday_status_id)])
         leave_type = hr_holidays.name
-        company_id = employee_id
+        company_id = employee_id.company_id
         header = ''
         header += '<table border="0" cellpadding="0" cellspacing="0" style="padding-top: 16px; background-color: #F1F1F1; font-family:Verdana, Arial,sans-serif; color: #454748; width: 100%; border-collapse:separate;">'                      
         header +=   '<tr>'
