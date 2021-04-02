6b56305844b33b77058991520d6e94e63f054cda
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-02 17:49:44 +0300

    Update create_refuse_comment.py

--- wizards/create_refuse_comment.py
+++ wizards/create_refuse_comment.py
@@ -19,11 +19,6 @@ class CreateLeaveComment(models.TransientModel):
             active_id = self.env.context.get('active_id')
         else:
             active_id = self.id
-            
-        leave_self = self.env['hr.leave'].search([('id', '=', active_id)], limit=1)
-
-        # leave_self.is_refused_user_id = False
-        comment =  self.env['create.refuse.comment'].sudo().search([('id', '=', new.id)], limit=1).comment
         for l2 in leave_self.leave_approvals: 
             # direct manager
             if l2.validators_type == 'direct_manager' and leave_self.employee_id.parent_id.id != False:
@@ -44,6 +39,8 @@ class CreateLeaveComment(models.TransientModel):
             # if not(l2.approval != True or (l2.approval == True and l2.validation_status == True)): 
             #     break     
         if approval_access:
+            leave_self = self.env['hr.leave'].search([('id', '=', active_id)], limit=1)
+            comment =  self.env['create.refuse.comment'].sudo().search([('id', '=', new.id)], limit=1).comment
             for holiday in leave_self:
                 if holiday.state not in ['confirm', 'validate', 'validate1']:
                     raise UserError(_(
@@ -61,11 +58,11 @@ class CreateLeaveComment(models.TransientModel):
                 holiday.linked_request_ids.action_refuse()
             leave_self._remove_resource_leave()
             leave_self.activity_update()
-            for user_obj in leave_self.leave_approvals:
+            for user_obj in leave_self.leave_approvals:    
                 if user_obj.validators_type == 'direct_manager' and leave_self.employee_id.parent_id.id != False:
                     if leave_self.employee_id.parent_id.user_id.id != False:
                         if leave_self.employee_id.parent_id.user_id.id == current_uid:
-                            validation_obj = leave_self.leave_approvals.sudo().search(
+                            validation_obj = leave_self.leave_approvals.search(
                                     [('id', '=', user_obj.id)])
                             validation_obj.validation_status = False
                             validation_obj.validation_refused = True
@@ -73,14 +70,14 @@ class CreateLeaveComment(models.TransientModel):
                 if  user_obj.validators_type == 'position':
                     employee = self.env['hr.employee'].sudo().search([('multi_job_id','in',user_obj.holiday_validators_position.id),('user_id','=',current_uid)])
                     if len(employee) > 0:
-                        validation_obj = self.leave_approvals.sudo().search(
+                        validation_obj = leave_self.leave_approvals.search(
                                     [('id', '=', user_obj.id)])
                         validation_obj.validation_status = False
                         validation_obj.validation_refused = True
                         validation_obj.leave_comments = comment
                 if  user_obj.validators_type == 'user':
                     if user_obj.holiday_validators_user.id == current_uid:
-                        validation_obj = self.leave_approvals.sudo().search(
+                        validation_obj = leave_self.leave_approvals.search(
                                     [('id', '=', user_obj.id)])
                         validation_obj.validation_status = False
                         validation_obj.validation_refused = True
