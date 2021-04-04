4555c2bdc4279ae77705ef64c7ef69c2d91e96e4
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-04 14:31:30 +0300

    Update create_leave_comment.py

--- wizards/create_leave_comment.py
+++ wizards/create_leave_comment.py
@@ -22,49 +22,48 @@ class CreateLeaveComment(models.TransientModel):
         comment =  self.env['create.leave.comment'].sudo().search([('id', '=', new.id)], limit=1).comment 
         all_emails = ""
         for user_obj in user.leave_approvals:
-            if user_obj.validation_status != True:
-                if user_obj.validators_type == 'direct_manager' and user.employee_id.parent_id.id != False:
-                    if user.employee_id.parent_id.user_id.id != False:
-                        raise exceptions.ValidationError(user.employee_id.parent_id.user_id.login)
-                        if all_emails != "":
-                            if str(user.employee_id.parent_id.user_id.login) not in all_emails:
-                                all_emails = all_emails + "," +str(user.employee_id.parent_id.user_id.login)
-                        else:
-                            all_emails = str(user.employee_id.parent_id.user_id.login)
-                        if user.employee_id.parent_id.user_id.id == current_uid:
-                            validation_obj = user.leave_approvals.search(
-                                    [('id', '=', user_obj.id)])
-                            validation_obj.validation_status = True
-                            validation_obj.validation_refused = False
-                            validation_obj.leave_comments = comment
-                if  user_obj.validators_type == 'position':
-                    employee = self.env['hr.employee'].sudo().search([('multi_job_id','in',user_obj.holiday_validators_position.id),('user_id','=',current_uid)])
-                    # for emp in employee:
-                    #     if all_emails != False:
-                    #         if str(emp.user_id.login) not in all_emails:
-                    #             all_emails = all_emails + "," +str(emp.user_id.login)
-                    #     else:
-                    #         all_emails = str(emp.user_id.login)
-                    if len(employee) > 0:
+            # if user_obj.validation_status != True:
+            if user_obj.validators_type == 'direct_manager' and user.employee_id.parent_id.id != False:
+                if user.employee_id.parent_id.user_id.id != False:
+                    if all_emails != "":
+                        if str(user.employee_id.parent_id.user_id.login) not in all_emails:
+                            all_emails = all_emails + "," + str(user.employee_id.parent_id.user_id.login)
+                    else:
+                        all_emails = str(user.employee_id.parent_id.user_id.login)
+                    if user.employee_id.parent_id.user_id.id == current_uid:
                         validation_obj = user.leave_approvals.search(
-                                    [('id', '=', user_obj.id)])
+                                [('id', '=', user_obj.id)])
                         validation_obj.validation_status = True
                         validation_obj.validation_refused = False
                         validation_obj.leave_comments = comment
-                if  user_obj.validators_type == 'user':
-                    # if all_emails != "":
-                    #     if str(user_obj.holiday_validators_user.login) not in all_emails:
-                    #         all_emails = all_emails + "," +str(user_obj.holiday_validators_user.login)
-                    # else:
-                    #     all_emails = str(user_obj.holiday_validators_user.login)
-                    if user_obj.holiday_validators_user.id == current_uid:
-                        validation_obj = user.leave_approvals.search(
-                                    [('id', '=', user_obj.id)])
-                        validation_obj.validation_status = True
-                        validation_obj.validation_refused = False
-                        validation_obj.leave_comments = comment
-                if not(user_obj.approval != True or (user_obj.approval == True and user_obj.validation_status == True)): 
-                    break 
+            if  user_obj.validators_type == 'position':
+                employee = self.env['hr.employee'].sudo().search([('multi_job_id','in',user_obj.holiday_validators_position.id),('user_id','=',current_uid)])
+                for emp in employee:
+                    if all_emails != False:
+                        if str(emp.user_id.login) not in all_emails:
+                            all_emails = all_emails + "," +str(emp.user_id.login)
+                    else:
+                        all_emails = str(emp.user_id.login)
+                if len(employee) > 0:
+                    validation_obj = user.leave_approvals.search(
+                                [('id', '=', user_obj.id)])
+                    validation_obj.validation_status = True
+                    validation_obj.validation_refused = False
+                    validation_obj.leave_comments = comment
+            if  user_obj.validators_type == 'user':
+                if all_emails != "":
+                    if str(user_obj.holiday_validators_user.login) not in all_emails:
+                        all_emails = all_emails + "," +str(user_obj.holiday_validators_user.login)
+                else:
+                    all_emails = str(user_obj.holiday_validators_user.login)
+                if user_obj.holiday_validators_user.id == current_uid:
+                    validation_obj = user.leave_approvals.search(
+                                [('id', '=', user_obj.id)])
+                    validation_obj.validation_status = True
+                    validation_obj.validation_refused = False
+                    validation_obj.leave_comments = comment
+            if not(user_obj.approval != True or (user_obj.approval == True and user_obj.validation_status == True)): 
+                break 
             user.all_emails = all_emails        
 
 
